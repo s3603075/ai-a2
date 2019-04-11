e7ca409550ae31b00b21e46d955d14ef9574e67b
rmit-s3603075-danny-ho <s3603075@student.rmit.edu.au>
2019-04-11 15:11:37 +1000

    horizontal heuristic

--- connectfour/agents/agent_student.py
+++ connectfour/agents/agent_student.py
@@ -1,10 +1,12 @@
 from connectfour.agents.computer_player import RandomAgent
 import random
+import copy
 
 class StudentAgent(RandomAgent):
     def __init__(self, name):
         super().__init__(name)
-        self.MaxDepth = 1
+        self.MaxDepth = 2
+        # self.NextState = None
 
 
     def get_move(self, board):
@@ -16,6 +18,7 @@ class StudentAgent(RandomAgent):
             A tuple of two integers, (row, col)
         """
 
+        # self.NextState = copy.deepcopy(board)
         valid_moves = board.valid_moves()
         vals = []
         moves = []
@@ -26,6 +29,8 @@ class StudentAgent(RandomAgent):
             vals.append( self.dfMiniMax(next_state, 1) )
 
         bestMove = moves[vals.index( max(vals) )]
+        print(bestMove)
+        
         return bestMove
 
     def dfMiniMax(self, board, depth):
@@ -46,6 +51,9 @@ class StudentAgent(RandomAgent):
                 
             moves.append( move )
             vals.append( self.dfMiniMax(next_state, depth + 1) )
+            #max(vals)
+            #alpha = max(alpha,value)
+            #if alpha > beta: break
 
         
         if depth % 2 == 1:
@@ -54,8 +62,38 @@ class StudentAgent(RandomAgent):
             bestVal = max(vals)
 
         return bestVal
+    
+    def score(self, array, board):
+        return 0
+    
+    def evaluateBoardState(self, board):        
+        # Check if winning piece/move
+        if board.winner() == self.id:
+            return 1
+        elif board.winner() == self.id % 2 + 1:
+            return -1
 
-    def evaluateBoardState(self, board):
+        # Check if draw
+        if board.terminal():
+            return 0
+
+        # Check row 4 piece position
+        # Check for each winning position
+        score = 0
+        for row in range(board.height):
+            for col in range(board.width - (board.num_to_connect - 1)) :
+                checkRow = board.board[row][col:col + board.num_to_connect]
+                if checkRow.count(self.id) == 3 and checkRow.count(0) == 1:
+                    score += 10
+                elif checkRow.count(self.id) == 2 and checkRow.count(0) == 2:
+                    score += 3
+                elif checkRow.count(self.id % 2 + 1) == 3 and checkRow.count(0) == 1:
+                    score -= 5  
+
+
+        return score
+        # Check column 4 piece position
+       
         """
         Your evaluation function should look at the current state and return a score for it. 
         As an example, the random agent provided works as follows:
@@ -88,5 +126,5 @@ class StudentAgent(RandomAgent):
             winner()
         """
 				
-        return random.uniform(0, 1)
+        # return random.uniform(0, 1)
 
